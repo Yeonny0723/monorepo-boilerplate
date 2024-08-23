d13cddd88c408ee66837f4629a1f2bfc8a6a24bd
yeonny0723 <kkjuyeon@gmail.com>
2024-08-23 15:52:00 +0900

    fix: 로딩 상태 관리
    - 페이지 단위 -> loading.tsx
    - 컴포넌트 단위 -> Suspense

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,9 +1,27 @@
-import { Metadata } from "next";
+import Link from "next/link";
 
-export const metadata: Metadata = {
-  title: "Home",
-};
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Home() {
-  return <div>Home</div>;
+async function getMovies() {
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+
+interface IMovie {
+  id: string;
+  title: string;
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+  return (
+    <div>
+      {movies.map((movie: IMovie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- apps/web/app/(movies)/movies/[id]/page.tsx
+++ apps/web/app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,21 @@
-export default function MovieDetail({
-  // server component by default
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  console.log(id);
-  return <h1>Movie {id}</h1>;
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
